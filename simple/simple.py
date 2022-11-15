40e5aa80c36cd431bd30372267439982a712fb4d
Horazdominguez <horazdominguez@gmail.com>
2022-11-14 23:47:08 -0300

    Cambios menores

--- simple/simple.py
+++ simple/simple.py
@@ -15,7 +15,7 @@ def rsimple(e):
     print("La persona de mayor edad es la que posee el ID: ", mayor_edad) #Imprimo en pantalla el Id de la persona de mayor edad
     menor_edad=sorted(e, key=lambda x: x["edad"])[1]                      #Imprimo en pantalla el Id de la persona de menor edad
     print("La persona de menor edad es la que posee el ID: ", menor_edad)                                     
-    for i in range(10):                             #Muestro la lista ordenada por id de mayor a menor
+    for i in range(10):                           			  #Muestro la lista ordenada por id de mayor a menor
         print(e[9-i])
 
 
