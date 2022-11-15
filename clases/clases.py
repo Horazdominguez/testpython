a480421e5dacd9b19814f93b767523a74e3f7226
Horazdominguez <horazdominguez@gmail.com>
2022-11-15 01:31:18 -0300

    Agrego ultimos detalles

--- clases/clases.py
+++ clases/clases.py
@@ -1,25 +1,46 @@
 class circulo():
-	def __init__(self, radio):
+	def __init__(self, radio):				#Defino la clase circulo
 		if radio < 0:
 			print("Ingrese un valor real")
 		else:
 			self.radio = radio
 		
 		
-	def area(self):
+	def area(self):						#Agrego el metodo Area
 		try:
-			area = 3.14 * (self.radio) ** 2
-			print("El area es: ",area)
+			area = 3.14 * (self.radio) ** 2		#Uso try para las excepciones que me puede dar 
+			print("El area es: ",area)		
 		except:
 			None
 			
-	def perimetro(self):
+	def perimetro(self):					#Agrego el metodo perimetro
 		try:
 			perimetro = 2*3.14*self.radio
 			print("El Perimetro es: ",perimetro)
 		except:
 			None	
+			
+	def __str__(self):					#Agrego __str__ para que me devuelva un intento de dibujo cuando lo imprimo
+		try:
+			if self.radio > 0:
+				print("--------")
+				print("|      |")
+				print("|      |")
+				print( " _______")
+				print("Parece un circulo")
+				return "Use la imaginaciòn"
+			else:
+				return ""			 #Agrego esta linea para que no me de error cuando ingresas radio = 0
+		except:
+			return "No hay circulo"
+			
+			
+R = int(input("Ingrese el Radio del circulo: "))		#Pedimos el ingreso del Radio
+circulo1 = circulo(R)						#Creamos el objeto circulo1 del tipo circulo
+circulo1.area()							#Llamamos al metodo area
+circulo1.perimetro()						#Lamamos al metodo perimetro
+print()
+print()
+print(circulo1)							#Imprimimos la representacion del circulo que nos devuelve str
 
-circulo1 = circulo(-13)
-circulo1.area()
-circulo1.perimetro()
+
